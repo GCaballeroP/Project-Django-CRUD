50a42c331f5c8ad1deb0bf4b10e21b6cd6564df1
Giuliana Caballero Pacheco <g.caballeropacheco@gmail.com>
2022-10-13 16:51:43 -0500

    modify file

--- agenda/settings.py
+++ agenda/settings.py
@@ -82,14 +82,12 @@ WSGI_APPLICATION = 'agenda.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'agenda',
-        'USER': 'postgres',
-        'PASSWORD': 'root',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
+    'default': dj_database_url.config (
+        # default='postgresql://postgres:postgres@localhost:5432/mysite', conn_max_age=600
+        default='sqlite:///db.sqlite3',
+        conn_max_age=600
+        
+    )
 }
 
 
